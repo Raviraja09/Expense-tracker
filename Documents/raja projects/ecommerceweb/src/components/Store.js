d2d94c17ac40e1c3afa2cf131b4d3e1188fb464f
Raviraja09 <rajavysyaraju09@gmail.com>
2023-02-20 20:47:33 +0530

    initialization

---
+++ Documents/raja projects/ecommerceweb/src/components/Store.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Store = () => {
+  return (
+    <div>
+      <Header
+       handleCartButtonClick={handleCartButtonClick} 
+      cartItems={cartItems} 
+      showCart={showCart} />
+      <h1>sdfvjd </h1>
+    </div>
+  );
+};
+export default Store;
